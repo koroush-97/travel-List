4286513c0ced6ea906b1ab4d60259c0ef5db7af3
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-07 16:28:29 +0330

    sorting function already

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Item from "./Item";
 
 export default function PackingList({ items, onDeleteItem, oneToggleItem }) {
+  const [sortBy, setStortBy] = useState("input");
+
+  let sortedItem;
+
+  if (sortBy === "input") sortedItem = items;
+  if (sortBy === "description")
+    sortedItem = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItem = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list" style={{}}>
       <ul style={{ overflow: "hidden" }}>
-        {items.map((item) => (
+        {sortedItem.map((item) => (
           <Item
             key={item.id}
             onDeleteItem={onDeleteItem}
@@ -17,7 +31,7 @@ export default function PackingList({ items, onDeleteItem, oneToggleItem }) {
       </ul>
 
       <div className="actions">
-        <select>
+        <select value={sortBy} onChange={(e) => setStortBy(e.target.value)}>
           <option value="input"> Sort by input order </option>
           <option value="description"> Sort by description </option>
           <option value="packed"> Sort by status </option>
