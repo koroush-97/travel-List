07fad3d14c8848b15a85bf734cb4c52aecc45e28
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-15 23:14:17 +0330

    set emoje for Order List

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -37,7 +37,7 @@ export default function PackingList({
 
       <div className="actions">
         <select value={sortBy} onChange={(e) => setStortBy(e.target.value)}>
-          <option value="input"> Sort by input order </option>
+          <option value="input"> Sort by input order 📑 </option>
           <option value="description"> Sort by description </option>
           <option value="packed"> Sort by status </option>
         </select>
